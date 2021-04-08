30d45589f6ae9601e19a3cf18ffdc65b8a6fa28b
MahyarNV <behzadimahyar85@gmail.com>
2021-04-08 09:49:29 +0430

    vstack and concatenate practice

--- src/2 shapeArray/main.py
+++ src/2 shapeArray/main.py
@@ -4,5 +4,18 @@ heights = [189, 170, 189, 163, 183, 171, 185, 168, 173, 183, 173, 173, 175, 178,
 ages = [57, 61, 57, 57, 58, 57, 61, 54, 68, 51, 49, 64, 50, 48, 65, 52, 56, 46, 54, 49, 51, 47, 55, 55, 54, 42, 51, 56, 55, 51, 54, 51, 60, 62, 43, 55, 56, 61, 52, 69, 64, 46, 54, 47, 70]
 
 multipleArr = heights + ages
+newArr = np.array(multipleArr).reshape((2, 45))
+print(newArr)
+print(newArr.shape)
+print(newArr[0, 4])
+print(newArr[0, 1:4])
+print(newArr[0, :4])
+print(newArr[:, 3])
 
-print(np.array(multipleArr).reshape((2, 45)).shape)
+newHeights = np.array(heights).reshape((1, 45))
+newAges = np.array(ages).reshape((1, 45))
+newArr = np.vstack((newHeights, newAges))
+print(newArr)
+
+newArr = np.concatenate((newHeights, newAges), axis=1) #axis=0 => like vstack
+print(newArr)
