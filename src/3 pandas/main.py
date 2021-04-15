d2eda8e652a3b71dbdd0f1ecad4cd143630e2aed
MahyarNV <behzadimahyar85@gmail.com>
2021-04-15 22:11:44 +0430

    Update .gitignore

--- src/3 pandas/main.py
+++ src/3 pandas/main.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-ser = pd.Series({'a': 1, 2: 'b'})
-print(ser['a'], ser[2])
-
-print(pd.Series({'a': 1, 2: 'b'})['a'])
-
-print(ser.loc['a'])
+# ser = pd.Series({'a': 1, 2: 'b'})
+# print(ser['a'], ser[2])
+#
+# print(pd.Series({'a': 1, 2: 'b'})['a'])
+#
+# print(ser.loc['a'])
